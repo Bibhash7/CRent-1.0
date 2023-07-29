3c6e480af1b1cd7111fbf52e61604f93a3d330e5
Bibhash Ghosh <tranceispermanent@gmail.com>
2023-07-30 01:42:55 +0530

    "Added config file"

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,12 @@
+mssql = {
+    "host":"localhost",
+    "driver":"ODBC Driver 17 for SQL Server",
+    "server":"LAPTOP-2QIA7QPR",
+    "username":"root",
+    "password":"",
+    "database":"crent"
+}
+
+cassandra = {
+    "port":"9042"
+}
